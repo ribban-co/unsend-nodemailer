0149f7d1f76a70fb807f5d5fb25566baadd9b350
Wolfeii <williammelin720@gmail.com>
2025-02-26 20:19:14 +0100

    use sdk

--- src/transport.ts
+++ src/transport.ts
@@ -2,19 +2,21 @@ import { SentMessageInfo, Transport } from "nodemailer";
 import { version as VERSION } from '../package.json';
 import { UnsendTransporterOptions } from "./types/transport";
 import MailMessage from "nodemailer/lib/mailer/mail-message";
+import { Unsend } from "unsend";
+import { Address } from "nodemailer/lib/mailer";
+
+type NodeMailerAddress = string | Address | Array<string | Address> | undefined;
 
 export class UnsendTransport implements Transport<SentMessageInfo> {
     public name = 'UnsendMailTransporter';
     public version = VERSION;
 
-    private readonly apiKey: string;
-    private readonly apiUrl: string;
+    private unsend: Unsend;
 
     constructor(options: UnsendTransporterOptions) {
-        const { apiKey, apiUrl = 'https://app.unsend.dev' } = options;
+        const { apiKey, apiUrl } = options;
 
-        this.apiKey = apiKey;
-        this.apiUrl = apiUrl.endsWith('/') ? `${apiUrl}api/v1` : `${apiUrl}/api/v1`;
+        this.unsend = new Unsend(apiKey, apiUrl);
     }
 
     public static makeTransport(options: UnsendTransporterOptions) {
@@ -26,37 +28,49 @@ export class UnsendTransport implements Transport<SentMessageInfo> {
             return callback(new Error('No subject, from or to address specified. Ensure that "subject", "from" and "to" fields are set.'), null);
         }
 
-        fetch(`${this.apiUrl}/emails`, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${this.apiKey}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                from: mail.data.from,
-                to: mail.data.to,
-                replyTo: mail.data.replyTo,
-                cc: mail.data.cc,
-                bcc: mail.data.bcc,
-                subject: mail.data.subject ?? '',
-                text: mail.data.text?.toString() ?? '',
-                html: mail.data.html?.toString() ?? '',
-                attachments: mail.data.attachments?.map((attachment) => ({
-                    filename: attachment.filename,
-                    content: attachment.content,
-                })),
-            })
+        this.unsend.emails.send({
+            from: mail.data.from as string,
+            to: this.toUnsendAddresses(mail.data.to),
+            replyTo: this.toUnsendAddresses(mail.data.replyTo),
+            cc: this.toUnsendAddresses(mail.data.cc),
+            bcc: this.toUnsendAddresses(mail.data.bcc),
+            subject: mail.data.subject ?? '',
+            text: mail.data.text?.toString() ?? '',
+            html: mail.data.html?.toString() ?? '',
+            attachments: mail.data.attachments?.map((attachment) => ({
+                filename: attachment.filename?.toString() ?? '',
+                content: attachment.content?.toString() ?? '',
+            })),
         }).then(async (response) => {
-            if (!response.ok) {
-                throw new Error(`[${response.status}]: ${response.statusText}`);
+            if (response.error) {
+                throw new Error(`[${response.error.code}]: ${response.error.message}`);
             }
 
-            const data = await response.json();
-
-            callback(null, data);
+            callback(null, response.data);
         }).catch((error) => {
             callback(error, null);
         });
     };
-    
+
+    private toUnsendAddresses(address: NodeMailerAddress): Array<string> {
+        if (!address) {
+            return [];
+        }
+
+        if (typeof address === 'string') {
+            return [address];
+        }
+
+        if (Array.isArray(address)) {
+            return address.map((address) => {
+                if (typeof address === 'string') {
+                    return address;
+                }
+
+                return address.address;
+            });
+        }
+
+        return [address.address];
+    }
 }
