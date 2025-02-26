c918cca9b1b25490c13d736b01e26c6f47ea0002
Wolfeii <williammelin720@gmail.com>
2025-02-26 14:01:55 +0100

    export from main

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { UnsendTransport } from './transport';
+import { UnsendTransporterOptions } from './types/transport';
+
+export { UnsendTransport };
+export type { UnsendTransporterOptions };
